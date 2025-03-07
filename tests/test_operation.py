3d0cd12f6d6f1f127b5799eb281205e089f77ce5
GauravGhatuary <gauravghatuary@gmail.com>
2025-03-07 19:35:19 +0530

    Added src and tests

---
+++ tests/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_operation import add,subtract
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-2,-4)==-6
+
+def test_subtract():
+    assert subtract(2,3)==-1
+    assert subtract(-2,-4)==2
